0bc1cd657c284c9fba361c05c1282d1b5c22e729
shaalx <1316011195@qq.com>
2015-11-14 20:43:51 +0800

    auto

---
+++ 11-12/README.md
@@ -0,0 +1,25 @@
+#	Go 基础
+
+## 基本类型
+
+	Go的变量都有默认值
+
+	`类型	字节	默认值`
+
+	`bool  1	false
+
+	uint8	byte	1	0
+	uint16	2	0
+	uint32	4	0
+	uint64	8	0
+
+	int8	1	0
+	int16 rune	2	0
+	int32 int 	4	0
+	int64	8	0
+	
+	float32 	4	0.0
+	float64		8	0.0
+
+	complex32	4	0+0i
+	complex64	8	0+0i`
---
+++ 11-14/caller_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"runtime"
+	"strconv"
+	"testing"
+)
+
+func AssertEqual(exp interface{}, act interface{}, t *testing.T) {
+	_, file, line, _ := runtime.Caller(1)
+	if exp != act {
+		t.Error("expected ", exp, ", actual ", act, "\n", "line ", strconv.Itoa(line), " in file ", file)
+	}
+}
+
+func TestAssert(t *testing.T) {
+	AssertEqual("exp", "act", t)
+}
